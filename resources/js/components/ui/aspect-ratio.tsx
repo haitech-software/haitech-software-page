1a2735164fece0f2951b7102f4a124135a0cb361
Muhammad Said Alkhudri <alkhudrimsaid733@gmail.com>
2025-05-27 19:36:49 +0700

    landing page (belum jadi)

---
+++ resources/js/components/ui/aspect-ratio.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { AspectRatio as RadixAspectRatio } from '@radix-ui/react-aspect-ratio';
+
+export const AspectRatio = RadixAspectRatio;
+export type AspectRatioProps = React.ComponentProps<typeof RadixAspectRatio>;
